f44dedabae94f39ac3c1a23f99535016a0a436e9
vshnvinmdr <155386752+vshnvinmdr@users.noreply.github.com>
2024-01-02 22:43:50 +0530

    Create 2610.Convert an Array Into a 2D Array With Conditions.java

---
+++ 2610.Convert an Array Into a 2D Array With Conditions.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int mp[] = new int[nums.length + 1];
+
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        
+        for (int c : nums) {
+            
+            if (mp[c] == result.size()) {
+                result.add(new ArrayList<>());
+            }
+
+            result.get(mp[c]).add(c);
+            mp[c]++;
+        }
+
+        return result;
+    }
+}
